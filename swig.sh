573b5ed37d2e42f854c0d7fac42f9bd125d889b8
Clement Rey <cr.rey.clement@gmail.com>
2017-09-08 00:28:45 +0200

    swig > impemented nicer, generic autobuild script

--- swig.sh
+++ swig.sh
@@ -8,14 +8,20 @@ SFML_VERSION="2.4.0"
 CSFML="CSFML"
 CSFML_VERSION="2.4"
 
-## Audio
-swig -go -cgo -intgosize 64 -I"$PWD/CSFML/include" $PWD/CSFML/include/SFML/Audio/Audio.i
-CGO_LDFLAGS="-L$PWD/CSFML/lib -lcsfml-audio" CGO_CFLAGS="-I$PWD/CSFML/include" go install -x ./CSFML/include/SFML/Audio
+SFML_MODULES=(Audio Graphics Window)
 
-## Graphics
-swig -go -cgo -intgosize 64 -I"$PWD/CSFML/include" $PWD/CSFML/include/SFML/Graphics/Graphics.i
-CGO_LDFLAGS="-L$PWD/CSFML/lib -lcsfml-graphics" CGO_CFLAGS="-I$PWD/CSFML/include" go install -x ./CSFML/include/SFML/Graphics
+for m in "${SFML_MODULES[@]}"; do
+	mm=$(echo $m | tr '[:upper:]' '[:lower:]')
 
-## Window
-swig -go -cgo -intgosize 64 -I"$PWD/CSFML/include" $PWD/CSFML/include/SFML/Window/Window.i
-CGO_LDFLAGS="-L$PWD/CSFML/lib -lcsfml-window" CGO_CFLAGS="-I$PWD/CSFML/include" go install -x ./CSFML/include/SFML/Window
+	mkdir -p "$PWD/$mm"
+
+	echo -n "building bindings for SFML's $m module..."
+	cp "$m.i" "$PWD/$CSFML/include/$SFML/$m/$m.i"
+	swig -go -cgo -intgosize 64 -I"$PWD/$CSFML/include" "$PWD/$CSFML/include/$SFML/$m/$m.i" > /dev/null 2>&1
+	cp "$PWD/$CSFML/include/$SFML/$m/$mm.go" "$PWD/$CSFML/include/$SFML/$m/${m}_wrap.c" "$PWD/$mm"
+	echo " OK."
+
+	echo -n "compiling go package for SFML's $m module..."
+	CGO_LDFLAGS="-L$PWD/CSFML/lib -lcsfml-$mm" CGO_CFLAGS="-I$PWD/$CSFML/include" go install "./$mm" > /dev/null 2>&1
+	echo " OK."
+done
