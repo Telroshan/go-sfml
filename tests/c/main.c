f52a100d985ecd579c56fc5b4a1a99e156390bc4
Clement Rey <cr.rey.clement@gmail.com>
2017-09-07 22:43:36 +0200

    tests > imported simple csfml example + implemented associated make targets

---
+++ tests/c/main.c
@@ -0,0 +1,28 @@
+#include <SFML/Window.h>
+#include <SFML/Graphics.h>
+
+int main() {
+    sfVideoMode mode = {800, 600, 32};
+    sfRenderWindow* window;
+    sfEvent event;
+
+    /* Create the main window */
+    window = sfRenderWindow_create(mode, "SFML window", sfResize | sfClose, NULL);
+    if (!window)
+        return 1;
+
+    /* Start the game loop */
+    while (sfRenderWindow_isOpen(window)) {
+        /* Process events */
+        while (sfRenderWindow_pollEvent(window, &event)) {
+            /* Close window : exit */
+            if (event.type == sfEvtClosed)
+                sfRenderWindow_close(window);
+        }
+        /* Clear the screen */
+        sfRenderWindow_clear(window, sfBlack);
+    }
+
+    /* Cleanup resources */
+    sfRenderWindow_destroy(window);
+}
